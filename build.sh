809217b196f3f093c14d533808d95bd93daa82c4
Jimmy Xu <xjimmyshcn@gmail.com>
2018-10-12 23:31:31 +0800

    [master] replace gitbook-summary with liubin/gitbook-summary

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ echo "> ensure gitbook-cli was installed"
 gitbook --version 2>/dev/null || npm install gitbook-cli -g
 
 echo "> ensure gitbook-summary was installed"
-book sm --version 2>/dev/null || npm install -g gitbook-summary
+book sm --version 2>/dev/null || npm install -g liubin/gitbook-summary
 
 echo "> delete old SUMMARY.md and _book/"
 rm -rf SUMMARY.md _book dist/hyperform 2>/dev/null
